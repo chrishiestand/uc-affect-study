e3994af1727723dd6330747363aad710edcfb21a
Chris Hiestand <chrishiestand@gmail.com>
2016-03-07 12:57:50 -0800

    modify image questions per request
    update output version

--- src/constants/Firebase.js
+++ src/constants/Firebase.js
@@ -1,4 +1,4 @@
 export const TOP_LEVEL     = 'affectstudy';
 export const ENV           = 'test';
 export const FIREBASE_HOST = 'radiant-heat-7129.firebaseio.com';
-export const VERSION       = "1.0";
+export const VERSION       = 2;
