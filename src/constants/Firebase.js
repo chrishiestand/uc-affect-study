0a5fcf92aac8987960af1d6b03320b3d4bd18ec9
Chris Hiestand <chrishiestand@gmail.com>
2016-03-07 23:58:19 -0800

    replace "new" env with "test"

--- src/constants/Firebase.js
+++ src/constants/Firebase.js
@@ -1,4 +1,4 @@
 export const TOP_LEVEL     = 'affectstudy';
-export const ENV           = 'new';
+export const ENV           = 'test';
 export const FIREBASE_HOST = 'radiant-heat-7129.firebaseio.com';
 export const VERSION       = 3;
